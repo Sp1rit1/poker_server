db7d58cb1db42d44047b232604354d14b0359ea3
Dmitry Hramtsov <dhramcov562@gmail.com>
2025-04-14 11:14:21 +0500

    В репозиторий добавлены остальные файлы

---
+++ src/main/java/com/io/github/Sp1rit1/poker_server/PokerServerApplication.java
@@ -0,0 +1,13 @@
+package com.io.github.Sp1rit1.poker_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/io/github/Sp1rit1/poker_server/PokerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.github.Sp1rit1.poker_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
